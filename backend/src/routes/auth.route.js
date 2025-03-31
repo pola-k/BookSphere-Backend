b2350d6cd72f89c685abab5637448ef33316b5a6
abdulrafayl226825 <l226825@lhr.nu.edu.pk>
2025-03-31 23:01:07 +0500

    get user profile api created and tested

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { login, Logout, singup } from "../controllers/auth.controller.js"
 import { GetComments, CreateComment, DeleteComment, EditComment } from "../controllers/comments.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js"
-
+import GetUser from "../controllers/user.controller.js"
 const router = express.Router()
 
 router.post("/signup", singup)
@@ -11,9 +11,13 @@ router.post("/login", login)
 
 router.post("/logout", Logout)
 
+
 router.get("/get-comments", GetComments)
 router.post("/create-comment", CreateComment)
 router.delete("/delete-comment", DeleteComment)
 router.put("/update-comment", EditComment)
+
+// user ki profile ka data access karna 
+router.get("/profile/", GetUser)
 
 export default router
